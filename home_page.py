f8366ee48f365b3169184460a52aea0fd3a66410
ReyBahl <bahl.reyansh@gmail.com>
2020-11-24 11:06:27 -0500

    First commit

---
+++ home_page.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template,  redirect,  url_for, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def start():
+    return redirect(url_for("home"))
+
+@app.route('/home')
+def home():
+    return render_template('homepage.html')
+
+@app.route('/login',  methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+
+        req = request.form
+        print(req)
+
+    return render_template('loginpage.html')
+
+@app.route('/takequiz', methods= ["GET", "POST"] )
+def takequiz():
+    if request.method == "POST":
+        req = request.form
+
+    return render_template('quizpage.html')
+
+app.run()
