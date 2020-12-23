40dae7046e4fd8f410338494021046ebf0b55a58
ReyBahl <bahl.reyansh@gmail.com>
2020-12-23 11:21:39 -0500

    Added answer checking for all types, not completely working yet

--- checkanswers.py
+++ checkanswers.py
@@ -12,20 +12,40 @@ def convert_to_dict(_list):
 
 def check(questions, responses):
     responses = convert_to_dict(responses)
+    print(responses)
+    results = []
     for x in questions:
-        if x['type'] == 'fillblank' or x['type'] == 'true_false' or x['type'] == 'mult_choice':
-            pass   
+        if x['type'] == 'fillblank':
+            results.append({'type': x['type'], 
+            'question': x['name'], 
+            'answer': responses[x['type']][0], 
+            'correct' : x['answer'], 
+            'boolcorrect' : responses[x['type']] == x['answer'][0]})    
         elif x['type'] == 'checkbox':
             for correct_choice in x['answer']:
                 correct_choice.replace(' ', '')
             for selected in responses['checkbox']:
                 selected.replace(' ', '')
-                
+
             correct = sorted(x['answer']) if type(x['answer']) == 'list' else x['answer']
             if len(responses['checkbox']) == 1:
                 response = responses['checkbox'][0] 
             else:
                 response = sorted(responses['checkbox'])
-            print(correct, "\n", response)
+            results.append({'type': x['type'], 
+            'question': x['name'], 
+            'answer': response, 
+            'correct' : correct_choice, 
+            'boolcorrect' : bool(response == correct_choice)})
+        elif x['type'] == 'mult_choice' or x['type'] == 'true_false':
+            results.append({'type': x['type'], 
+            'question': x['name'], 
+            'answer': responses[x['type']][0], 
+            'correct' : x['answer'], 
+            'boolcorrect' : responses[x['type']][0].lower() == x['answer'][0].lower()})
+        
+
+    print(results)
+
 
 #print(convert_to_dict([('checkbox', '0'), ('checkbox', '1'), (' leadership', '2'), ('Question1', '0'), ('Question1', 'True'), ('Answer1', 'fasdf')]))
